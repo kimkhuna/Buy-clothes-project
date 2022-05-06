9618037d5b89b263deb8ec3ba966cfcc327a978c
kimkhuna <45312005+kimkhuna@users.noreply.github.com>
2022-05-06 12:58:39 +0900

    제출본

--- KKH_641/KKH_641Dlg.cpp
+++ KKH_641/KKH_641Dlg.cpp
@@ -213,28 +213,6 @@ HCURSOR CKKH_641Dlg::OnQueryDragIcon()
 
 
 
-void CKKH_641Dlg::OnStnClickedBitmapLogo()
-{
-	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
-}
-
-
-void CKKH_641Dlg::OnCbnSelchangeCombo1()
-{
-	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
-}
-
-
-void CKKH_641Dlg::OnEnChangeEdit2()
-{
-	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
-	// CDialogEx::OnInitDialog() 함수를 재지정 
-	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
-	// 이 알림 메시지를 보내지 않습니다.
-
-	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
-}
-
 
 void CKKH_641Dlg::OnClickedButtonList641()
 {
